dbdf3cbc68c15e2cc47766c9d8bcb7d88c5d3a65
Adam Soltys <asoltys@gmail.com>
2019-09-23 15:39:33 -0700

    better purgecss extractor

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,6 @@ import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
 import getPreprocessor from "svelte-preprocess";
 import postcss from "rollup-plugin-postcss";
-import PurgeSvelte from "purgecss-from-svelte";
 import path from "path";
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
@@ -33,7 +32,25 @@ const postcssPlugins = (purgecss = false) => {
         content: ["./**/*.svelte", "./src/template.html"],
         extractors: [
           {
-            extractor: PurgeSvelte,
+            extractor: content => {
+              const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+
+              const matchedTokens = [];
+
+              let match = regExp.exec(content);
+
+              while (match) {
+                if (match[0].startsWith("class:")) {
+                  matchedTokens.push(match[0].substring(6));
+                } else {
+                  matchedTokens.push(match[0]);
+                }
+
+                match = regExp.exec(content);
+              }
+
+              return matchedTokens;
+            },
 
             // Specify the file extensions to include when scanning for
             // class names.
