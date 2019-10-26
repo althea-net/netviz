42fbd5dc14b6f3e1d5ab2085ab79402a219f495d
Adam Soltys <asoltys@gmail.com>
2019-10-25 17:01:30 -0700

    set base path to /network

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
+    '/network',
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
