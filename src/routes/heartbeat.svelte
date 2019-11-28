4ac978b8c59fd856ec26a3f40a9fcb08b114cd6c
Adam Soltys <asoltys@gmail.com>
2019-11-27 23:19:09 -0800

    wip

--- src/routes/heartbeat.svelte
+++ src/routes/heartbeat.svelte
@@ -4,31 +4,21 @@
 
   onMount(async () => {
     let res = await fetch("/nodes");
-    let nodes = await res.json();
+    let json = await res.json();
     let minutes = Array.from(Array(60).keys());
-    let keys = Object.keys(nodes);
-    let values = Object.values(nodes);
+    let data = minutes.map(minute => ({
+      minute,
+      beats: Math.floor(Math.random() * 13)
+    }));
+    nodes = Object.keys(json);
 
     tick().then(() => {
-      keys.map(id => {
-        let data = minutes.map(minute => {
-          let start = minute * 60;
-          let end = (minute+1) * 60;
-          let pings = 0;
-          Object.keys(nodes[id]).filter(t => t > start && t <= end).map(k => pings += nodes[id][k]);
-          return { minute, pings }
-        });
-
-        data = [{ minute: 1, pings: 5 }];
-
+      nodes.map(id => {
         let ndx = crossfilter(data),
           dimension = ndx.dimension(d => d.minute),
-          group = dimension.group().reduceSum(d => d.pings);
+          group = dimension.group().reduceSum(d => d.beats);
 
-        console.log(ndx);
-
-        let chart = dc
-          .barChart("#a" + id.replace(/\W/g, ""))
+        let chart = dc.barChart("#a" + id.replace(/\W/g, ""))
           .x(d3.scaleLinear().domain([0, 60]))
           .brushOn(false)
           .xAxisLabel("minute")
