225c3f162cc49ff36a023b511ccf78da5c3c2bc7
Adam Soltys <asoltys@gmail.com>
2019-12-16 16:55:22 -0800

    use forms for keyboard submit

--- src/routes/heartbeat.svelte
+++ src/routes/heartbeat.svelte
@@ -3,7 +3,6 @@
   let keys = [];
   let names = {};
   let nodes;
-  let nodeList;
   let minutes = Array.from(Array(60).keys());
   let initialized = false;
   let organizer;
@@ -16,10 +15,9 @@
   };
 
   const getData = async () => {
-
     let res = await fetch("/network/heartbeats", {
       method: "POST",
-      body: JSON.stringify({ list: nodeList }),
+      body: JSON.stringify({ list: Object.keys(names) }),
       headers
     });
 
@@ -72,70 +70,87 @@
   };
 
   onMount(() => {
-    nodeList = JSON.parse(window.localStorage.getItem("heartbeatNodes")) || [];
+    names = JSON.parse(window.localStorage.getItem("names")) || {};
     getData();
     setInterval(getData, 5000);
   });
 
   let node;
   const addNode = () => {
-    nodeList.push(node);
-    window.localStorage.setItem("heartbeatNodes", JSON.stringify(nodeList));
+    names[node] = "";
+    window.localStorage.setItem("names", JSON.stringify(names));
     initialized = false;
   };
 
-const delNode = id => {
-  let i = nodeList.findIndex(n => n === id);
-  if (i > -1) nodeList.splice(i, 1);
-  window.localStorage.setItem("heartbeatNodes", JSON.stringify(nodeList));
-  initialized = false;
-} 
+  const delNode = id => {
+    delete names[id];
+    window.localStorage.setItem("names", JSON.stringify(names));
+    initialized = false;
+  };
 
-const loadNodes = async () => {
-  try {
-    let res = await fetch("/network/nodes", {
-      method: "POST",
-      body: JSON.stringify({ organizer, password }),
-      headers
-    });
+  const loadNodes = async e => {
+    try {
+      let res = await fetch("/network/nodes", {
+        method: "POST",
+        body: JSON.stringify({ organizer, password }),
+        headers
+      });
 
-    if (res.status === 401) failed = true;
+      if (res.status === 401) failed = true;
 
-    if (res.ok) {
-      failed = false;
-      let list = await res.json();
-      Object.keys(list).map(n => nodeList.push(n));
-      nodeList = nodeList.filter((v, i, self) => self.indexOf(v) === i);
-      window.localStorage.setItem("heartbeatNodes", JSON.stringify(nodeList));
+      if (res.ok) {
+        failed = false;
+        names = await res.json();
+        Object.keys(res).map(k => {
+          if (!names[k]) names[k] = res[k];
+        });
+        window.localStorage.setItem("names", JSON.stringify(names));
+      }
+    } catch (e) {
+      console.log("err", e);
     }
-  } catch (e) {
-    console.log("err", e);
-  }
-} 
+  };
 </script>
 
 <style>
-  .card { min-width: 550px; } 
-  .failed { border: 1px solid red; }
+  .failed {
+    border: 1px solid red;
+  }
 </style>
 
-<div class="w-full p-4">
-  <input bind:value={node} placeholder="WG Pubkey" />
-  <button on:click={addNode}>Add Node</button>
+<div class="w-full p-4 flex flex-wrap">
+  <form on:submit|preventDefault={addNode} class="mr-2 mb-2">
+    <input bind:value={node} placeholder="WG Pubkey" />
+    <button>Add Node</button>
+  </form>
 
-  <input bind:value={organizer} placeholder="Organizer Address" class:failed />
-  <input bind:value={password} placeholder="Password" type="password" class:failed />
-  <button on:click={loadNodes}>Load Nodes</button>
+  <form on:submit|preventDefault={loadNodes}>
+    <input
+      bind:value={organizer}
+      placeholder="Organizer Address"
+      class:failed />
+    <input
+      bind:value={password}
+      placeholder="Password"
+      type="password"
+      class:failed />
+    <button>Load Nodes</button>
+  </form>
 </div>
 
-  <div class="flex flex-wrap">
-    {#each keys as id (id)}
-      <div class="card">
-        <div class="flex">
-          <h1 class="text-lg text-center mr-2">{names[id] || id}</h1>
-          <img src="trash.svg" alt="Trash Can Icon" on:click={() => delNode(id)} style="width: 20px" class="ml-auto cursor-pointer" />
-        </div>
-        <div id={'a' + id.replace(/\W/g, '')} />
+<div class="flex flex-wrap -mx-2">
+  {#each keys as id (id)}
+    <div class="card w-full md:w-1/2 px-2">
+      <div class="flex">
+        <h1 class="text-lg text-center mr-2 break-all">{names[id] || id}</h1>
+        <img
+          src="trash.svg"
+          alt="Trash Can Icon"
+          on:click={() => delNode(id)}
+          style="width: 20px"
+          class="ml-auto cursor-pointer" />
       </div>
-    {/each}
-  </div>
+      <div id={'a' + id.replace(/\W/g, '')} />
+    </div>
+  {/each}
+</div>
